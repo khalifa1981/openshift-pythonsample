0f5dbe0bf446c89ae827d7a93167b2ab837e41a7
Mohamed Khalifa <31257356+khalifa1981@users.noreply.github.com>
2021-04-06 18:04:19 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route("/how are you")
+def hello():
+    return "Iam good, How anout you ?"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
